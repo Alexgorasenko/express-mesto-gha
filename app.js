7c0cd13f13d7a0fc654aeb758b37c29e01fbe36f
DESKTOP-89I0QNC\Администратор <alex.gorasenko1605@gmail.com>
2023-07-13 23:55:07 +0200

    Выполнение 14 проектной работы

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const cookieParser = require('cookie-parser');
 
 const router = require('./routes/index');
 
-const celebrate = require('./middlewares/celebrate');
-
 const error = require('./middlewares/error');
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
@@ -18,9 +16,8 @@ app.use(express.json());
 
 app.use(cookieParser());
 app.use(router);
-app.use(celebrate)
-app.use(error);
 
+app.use(error);
 
 app.listen(3000, () => {
   console.log('Сервер запущен!');
